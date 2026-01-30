55005c3f91b35db4455c82b32c043acd9b5ecf18
Rizwansh-dev <255385401+rizwanshaukat-dev@users.noreply.github.com>
2026-01-31 00:09:40 +0500

    Add coloring logic to the cell

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ const div=document.getElementById("container");
 function customizeGrid() {
     const size=parseInt(prompt("Type value to create grid. Maximum: 100 ","20"));
     if(size<=100){
+        div.innerHTML=" ";
         let totalSize= size * size;
         for(let i=0;i<totalSize;i++){
-            const cell= document.createElement('div')
+            const cell= document.createElement('div');
             cell.classList.add('cell');
+            cell.style.flex=`1 1 ${100/size}% `;
             div.append(cell);
         }
-    }
+    document.querySelectorAll(".cell").forEach(cell=>{
+    cell.addEventListener("mousemove",function(){
+    this.style.backgroundColor="red";});  
+    }); 
+}
 }
